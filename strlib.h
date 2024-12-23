d9548d4cb389ad609a1aea5e6a61bd4fc98e8e00
constantijn <cdemeer@fim.uni-passau.de>
2024-12-23 23:17:48 +0100

    replace

--- strlib.h
+++ strlib.h
@@ -11,6 +11,16 @@
 typedef void* (*Allocator) (size_t);
 #endif // ALLOCATOR
 
+#define STR_NUMARGS(...)  (sizeof((str[]){{0}, ##__VA_ARGS__})/sizeof(str)-1)
+#define STR_NOT_FOUND -1
+#define StrAlloc
+
+#ifdef STR_DEBUG
+#define str_info(msg, ...) (printf("%s:%d: " msg"\n", __FILE__, __LINE__, ## __VA_ARGS__))
+#else
+#define str_info(msg, ...)
+#endif // STR_DEBUG
+
 typedef struct{
 	char *value;
 	size_t len;
@@ -22,24 +32,33 @@ typedef struct{
 } str_pair;
 
 size_t strlib_len(char *s);
-void strlib_ncpy(char *s, size_t n, char *d);
+char* strlib_ncpy(char *s, size_t n, char *d);
 char* strlib_dup(char *s, Allocator alloc);
 void strlib_memset(char *d, char v, size_t n);
 
-str str_new(char *s, Allocator alloc);
-str str_dup(str string, Allocator alloc);
+StrAlloc str str_new(char *s, Allocator alloc);
+StrAlloc str str_dup(str string, Allocator alloc);
+StrAlloc str str_sub(Allocator alloc, str string, size_t from, size_t to);
+StrAlloc str_pair str_split(Allocator alloc, str string, char del);
+StrAlloc str str__concat(Allocator alloc, int n, ...);
+StrAlloc str str_replace_str(str string, str a, str b, Allocator alloc);
 char *str_to_buffer(str s, char *buffer, size_t buffer_size);
-str str_sub(Allocator alloc, str string, size_t from, size_t to);
-str_pair str_split(Allocator alloc, str string, char del);
 int str_find(str string, char c);
-str str__concat(Allocator alloc, int n, ...);
+int str_find_str(str string, str query);
+bool str_starts_with(str base, str start);
+bool str_ends_with(str base, str end);
+bool str_equals(str a, str b);
+str str_from(str string, size_t from);
+str str_peek(str string, size_t from, size_t to);
+size_t str_count(str string, char c);
+size_t str_count_str(str string, str s);
+void str_replace(str string, char a, char b);
 
 #define str(s) (str){.value=(s), .len=strlib_len((s))}
-#define STR_NUMARGS(...)  (sizeof((str[]){{0}, ##__VA_ARGS__})/sizeof(str)-1)
 #define str_concat(alloc, ...) (str__concat((alloc), STR_NUMARGS(__VA_ARGS__), ##__VA_ARGS__))
 #define str_print(str) (printf("\"%s\"\n", (str).value))
-
-#define STR_NOT_FOUND -1
+#define str_at(str, i) ((str).value[(i)])
+#define str_empty(str) ((str).len == 0)
 
 #endif // _STRLIB_H
 #ifdef STRLIB_IMPLEMENTATION
@@ -52,13 +71,14 @@ size_t strlib_len(char *s)
 	return r-s-1;
 }
 
-void strlib_ncpy(char *s, size_t n, char *d)
+char* strlib_ncpy(char *s, size_t n, char *d)
 {
-	if (s == NULL || d == NULL || n == 0) return;
+	if (s == NULL || d == NULL || n == 0) return d;
 	while (n > 0){
 		*d++ = *s++;
 		n--;
 	}
+	return d;
 }
 
 void strlib_memset(char *s, char v, size_t n)
@@ -149,6 +169,110 @@ int str_find(str string, char c)
 		if (string.value[i] == c) return i;
 	}
 	return STR_NOT_FOUND;
+}
+
+int str_find_str(str string, str query)
+{
+	if (str_empty(string) || str_empty(query) || query.len > string.len) return STR_NOT_FOUND;
+	for (size_t i=0; i<string.len-query.len+1; ++i){
+		if (str_starts_with(str_from(string, i), query)) return i;
+	}
+	return STR_NOT_FOUND;
+}
+
+bool str_starts_with(str base, str start)
+{
+	if (start.len > base.len) return false;
+	for (size_t i=0; i<start.len; ++i){
+		if (start.value[i] != base.value[i]) return false;
+	}
+	return true;
+}
+
+bool str_ends_with(str base, str end)
+{
+	if (end.len > base.len) return false;
+	size_t offset = end.len - base.len;
+	for (size_t i=0; i<end.len; ++i){
+		if (base.value[offset+i] != end.value[i]) return false;
+	}
+	return true;
+}
+
+bool str_equals(str a, str b)
+{
+	if (a.len != b.len) return false;
+	size_t len = a.len;
+	char *pa = a.value;
+	char *pb = b.value;
+	while (len-- > 0){
+		if (*pa++ != *pb++) return false;
+	}
+	return true;
+}
+
+str str_from(str string, size_t from)
+{
+	if (from >= string.len) return (str) {0};
+	return (str) {.value=string.value+from, .len=string.len-from};
+}
+
+str str_peek(str string, size_t from, size_t to)
+{
+	if (from >= to || to > string.len || from > string.len) return (str) {0};
+	return (str) {.value=string.value + from, .len=to-from};
+}
+
+size_t str_count(str string, char c)
+{
+	size_t count = 0;
+	for (size_t i=0; i<string.len; ++i){
+		if (string.value[i] == c) count++;
+	}
+	return count;
+}
+
+size_t str_count_str(str string, str s)
+{
+	size_t count = 0;
+	int index = 0;
+	int i;
+	while ((i = str_find_str(str_from(string, index), s)) != STR_NOT_FOUND){
+		count++;
+		index += i+1;
+	}	
+	return count;
+}
+
+void str_replace(str string, char a, char b)
+{
+	char *p;
+	for (size_t i=0; i<string.len; ++i){
+		p = string.value + i;
+		if (*p == a) *p=b;
+	}
+}
+
+str str_replace_str(str string, str a, str b, Allocator alloc)
+{
+	size_t count = str_count_str(string, a);
+	if (count == 0) return str_dup(string, alloc);
+	size_t delta = b.len - a.len;
+	size_t length = string.len + delta*count;
+	char *value = alloc(length+1);
+	char *w = value;
+	char *r = string.value;
+	for (size_t i=0; i<count; ++i){
+		char *n = r + str_find_str(str(r), a);
+		if (n-r > 0)
+		w = strlib_ncpy(r, n-r, w);
+		w = strlib_ncpy(b.value, b.len, w);
+		r = n+a.len;
+	}
+	if (r-string.value < length){
+		strlib_ncpy(r, length-(r-string.value), w);
+	}
+	return (str) {.value=value, .len=length};
 }
 
 #endif // STRLIB_IMPLEMENTATION
--- testing.c
+++ testing.c
@@ -1,8 +1,11 @@
 #define STRLIB_IMPLEMENTATION
+#define STR_DEBUG
 #include "strlib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+#define b(value) ((value)? "true": "false")
 
 void* c_alloc(size_t n)
 {
@@ -13,10 +16,12 @@ void* c_alloc(size_t n)
 
 int main(void)
 {
-	str hello = str_concat(c_alloc, str("hello"), str(" "), str("world"));
-	str_print(hello);
-	str_pair halfs = str_split(c_alloc, hello, ' ');
-	str_print(halfs.a);
-	str_print(halfs.b);
+	// str a = str("halloal al");
+	// printf("%d\n", str_find_str(str_from(a, 2), str("al")));
+	str a = str("Hallo World");
+	printf("%d -> %s\n", a.len, a.value);
+	str_print(a);
+	str_remove_mod(&a, 'l');
+	printf("%d -> %s\n", a.len, a.value);
 	return 0;
 }
